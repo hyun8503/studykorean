be1bc32a0e0b16fda9f98610f3ff3351e5d82032
jane <hyun8503@gmail.com>
2020-09-17 11:10:40 +0900

    Updates

---
+++ precache-manifest.66bf95f211c39a82c67f01faad8460f4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "16aea9261db2d26433109a8059bf592e",
+    "url": "/studykorean2/index.html"
+  },
+  {
+    "revision": "4ae762ea657b791f91ba",
+    "url": "/studykorean2/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "6754b9e83edd79c2d841",
+    "url": "/studykorean2/static/js/2.e021249d.chunk.js"
+  },
+  {
+    "revision": "882fee5ad56cfd78b1cc5d94b617aa42",
+    "url": "/studykorean2/static/js/2.e021249d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4ae762ea657b791f91ba",
+    "url": "/studykorean2/static/js/main.9174bf31.chunk.js"
+  },
+  {
+    "revision": "f5cab8951f61460f7c25",
+    "url": "/studykorean2/static/js/runtime-main.072e38f2.js"
+  }
+]);
